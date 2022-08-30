a2b79ff7a109dd0678030c7949deadc5f768a3bb
Francisco <francisco.riverosc@sansano.usm.cl>
2022-08-30 03:22:08 -0400

    a

--- bottelegram.js
+++ bottelegram.js
@@ -13,7 +13,7 @@ var con = mysql.createConnection({
     password: "",
     database: "tiendas"
   });*/
-var con =  mysql.connector.connect(
+var con = mysql.createConnection(
     host="db-mysql-nyc1-93755-do-user-12336633-0.b.db.ondigitalocean.com",
     user="doadmin",
     password="AVNS_fh12ouJEjX8o4mU-0xs",
