c1ef682c5f8dcc14dd0236e53d5219cf3fe42858
Francisco <francisco.riverosc@sansano.usm.cl>
2022-08-31 03:38:52 -0400

    aa

--- storescraper/stores/mercado_libre_chile.py
+++ storescraper/stores/mercado_libre_chile.py
@@ -783,10 +783,9 @@ class MercadoLibreChile(Store):
 
             price = Decimal(box_winner['price'])
             original_price = price
-            try:
+            if(box_winner['original_price'] != None and box_winner['original_price'] != "null"):
                 original_price = box_winner['original_price']
-            except:
-                original_price = price
+
             stock = int(box_winner['available_quantity'])
 
             seller_endpoint = 'https://api.mercadolibre.com/users/' \
