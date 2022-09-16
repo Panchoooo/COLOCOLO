5671bc36df5fb149bdb9e8b5a2d97988215812a6
Francisco <francisco.riverosc@sansano.usm.cl>
2022-09-16 01:20:50 -0300

    a

--- Tiendas/Paris.js
+++ Tiendas/Paris.js
@@ -257,7 +257,7 @@ async function almacenar(Productos){
             rs = await fquery('SELECT best_price from tiendasv2 where store = ? and keey = ?',[Producto[0],Producto[2]]);
             if(rs.length>0  && rs[0].best_price != Producto[10] ){
                 //console.log("Actualizacion de Producto "+Producto[2])
-                ra = await fquery('INSERT INTO tiendas_log (store,keey,price,fecha) VALUES (?,?,?,NOW())  ',[Productos[0],Producto[2],Producto[10]]);
+                ra = await fquery('INSERT INTO tiendas_log (store,keey,price,fecha) VALUES (?,?,?,NOW())  ',[Producto[0],Producto[2],Producto[10]]);
                 ra = await fquery('UPDATE tiendasv2 SET best_price = ?,normal_price = ?, last_date = NOW() WHERE store = ? and keey = ?  ',[Producto[10],Producto[8],Producto[0],Producto[2]]);
                 var dif = 100-Producto[10]*100/rs[0].best_price;
                 if(rs[0].best_price > Producto[10] && (dif>30)){
